0a343c57baa092d7f32b499363bb2ec1c6f0317a
ogphyuri <64627311+ogphyuri@users.noreply.github.com>
2024-06-03 23:21:20 -0700

    Add files via upload

---
+++ Parameterization.py
@@ -0,0 +1,36 @@
+import math
+
+class Parameterization:
+    def __init__(self):
+        """
+        if user need needs to initialize certain attributes or set up
+        you can add something in here.
+        """
+        pass
+    
+    #you can feel free to add function
+    def calculate_volume_sphere(self, radius):
+        """
+        Calculate the volume of a sphere. The argument is radius
+        The argument 'radius' can take any numeric value using parameterization.
+        it returns volume of sphere.
+        """
+        if radius < 0:
+            raise ValueError("Negative number not allowed.")
+        
+        #formula for volume of Sphere
+        #V = 4/3 π r³
+        #Using import math
+        volume = (4/3) * math.pi * radius**3 
+        return volume
+
+# Main function to declare radius variable
+#Example to run the code
+if __name__ == "__main__":
+    foo = Parameterization()
+    radius = 3
+    radius_f = 65.12
+    volume = foo.calculate_volume_sphere(radius)
+    volume_f = foo.calculate_volume_sphere(radius_f)
+    print("Radius value: {}\nVolume of sphere: {}\n".format(radius,volume))
+    print("Radius value: {}\nVolume of sphere: {}".format(radius_f,volume_f))
